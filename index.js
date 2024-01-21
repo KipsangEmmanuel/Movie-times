8024ec8c228addfb5ae1b3eb258f468897fafb44
unknown <emmqnuelkipsqng@gmail.com>
2024-01-21 18:59:33 +0300

    finalised

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const list = document.querySelector('#movie-list ul')
+    const list = document.querySelector('#movie-list ul');
     const forms = document.forms;
 
+    const storedMovies = JSON.parse(localStorage.getItem('movies')) || [];
+
+    storedMovies.forEach((item)=> {
+        appendToList(item);
+    });
 
     list.addEventListener('click', function(e) {
-        e.preventDefault()
-        if(e.target.className == 'delete') {
+        e.preventDefault();
+        if (e.target.className == 'delete') {
             const li = e.target.parentElement;
             li.parentNode.removeChild(li);
+            const movieName = li.querySelector('.name').textContent;
+            removeItemFromLocalStorage(movieName);
+        
+            updateLocalStorage();
         }
-    })
+    });
+
+    
+    function appendToList(movieObject) {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span class="name">${movieObject.name}</span>
+            <span class="date">${movieObject.date}</span>
+            <span class="delete">delete</span>
+        `;
+        list.appendChild(li);
+    }
 
     const addForm = forms['add-movie'];
+
     addForm.addEventListener('submit', function(e) {
         e.preventDefault();
-
-        //create elements
         const value = addForm.querySelector('input[type="text"]').value;
+        const movieDate = new Date().toLocaleString();
+
+        const movieObject = {
+            name: value,
+            date: movieDate
+        };
+
+        storedMovies.push(movieObject);
+
+        updateLocalStorage();
+
+        appendToList(movieObject);
+        });
+
+    function updateLocalStorage() {
+        localStorage.setItem('movies', JSON.stringify(storedMovies));
+    }
+});
 
 
-        const li = document.createElement('li');
-        const movieName = document.createElement('span');
-        const movieDate = document.createElement('span');
-        const deleteBtn = document.createElement('span');
-
-        movieName.textContent = value;
-        localStorage.setItem('movies', JSON.stringify(value))
-
-        // const newDate = new Date()
-
-        movieDate.textContent = new Date().toLocaleString()
-        deleteBtn.textContent = 'delete';
-
-        movieName.classList.add('name');
-        movieDate.classList.add('date');
-        deleteBtn.classList.add('delete');
-
-
-        li.appendChild(movieName);
-        li.appendChild(movieDate);
-        li.appendChild(deleteBtn);
-        list.appendChild(li);
-
-
-        addForm.querySelector('input[type="text"]').value = '';
-
-    })
-
-})
